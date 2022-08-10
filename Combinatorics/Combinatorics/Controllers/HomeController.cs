8911165ac6dae0ad56697d141d787e705130f5b5
unknown <talktoitopa@gmail.com>
2022-08-10 09:59:45 +0100

    csharp webapp

---
+++ Combinatorics/Combinatorics/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Combinatorics.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Combinatorics.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Permutation(int n, int r)
+        {
+            this.ViewBag.p = n;
+            this.ViewBag.r = r;
+            this.ViewBag.permutation = nPr(n, r);
+            return View("Index");
+
+        }
+
+        public ActionResult Combination(int n, int r)
+        {
+            this.ViewBag.p = n;
+            this.ViewBag.r = r;
+            this.ViewBag.combination = nCr(n, r);
+            return View("Index");
+
+        }
+
+        public static long nCr(int n, int r)
+        {
+            // naive: return Factorial(n) / (Factorial(r) * Factorial(n - r));
+            return nPr(n, r) / Factorial(r);
+        }
+
+        public static long nPr(int n, int r)
+        {
+            // naive: return Factorial(n) / Factorial(n - r);
+            return FactorialDivision(n, n - r);
+        }
+
+        private static long FactorialDivision(int topFactorial, int divisorFactorial)
+        {
+            long result = 1;
+            for (int i = topFactorial; i > divisorFactorial; i--)
+                result *= i;
+            return result;
+        }
+
+        private static long Factorial(int i)
+        {
+            if (i <= 1)
+                return 1;
+            return i * Factorial(i - 1);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Combinatorics/Combinatorics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Combinatorics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
